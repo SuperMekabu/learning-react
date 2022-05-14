fc860209a32b5e49bc3dbc070e3d9dca0d7719e3
SuperMekabu <yuki.military@gmail.com>
2022-05-14 17:28:39 +0900

    useState vs Recoil2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React, { FC, useMemo, useState } from "react";
 import "./App.css";
-import { atom, RecoilRoot, RecoilState, useRecoilState } from "recoil";
+import {
+  atom,
+  RecoilRoot,
+  RecoilState,
+  SetterOrUpdater,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
 function App() {
+  console.log("OUter redraw");
   return (
     <RecoilRoot>
       <div className="App">
@@ -10,7 +19,9 @@ function App() {
           <p>AAAA</p>
           <Inner />
           <Inner2Wrapper />
+          <Inner2_2Wrapper />
           <Inner3Wrapper />
+          <Inner3_2Wrapper />
           <Inner4Wrapper />
         </main>
       </div>
@@ -19,11 +30,12 @@ function App() {
 }
 
 const Inner = () => {
-  console.log("redraw");
+  console.log("Inner redraw");
   const [state, setState] = useState(0);
 
   return (
     <div>
+      <h3>Inner</h3>
       <button onClick={() => setState((prev) => prev + 1)}>increment</button>
       <p>{state}</p>
     </div>
@@ -36,6 +48,7 @@ const Inner2Wrapper = () => {
 
   return (
     <div>
+      <h3>Inner2</h3>
       <button onClick={() => setState((prev) => prev + 1)}>increment</button>
       <Inner2 state={state} />
     </div>
@@ -51,6 +64,31 @@ const Inner2: FC<{ state: number }> = ({ state }) => {
   );
 };
 
+const Inner2_2Wrapper = () => {
+  console.log("Inner2_2Wrapper redraw");
+  const [state, setState] = useState(0);
+
+  return (
+    <div>
+      <Inner2_2 state={state} setState={setState} />
+    </div>
+  );
+};
+
+const Inner2_2: FC<{ state: number; setState: React.Dispatch<React.SetStateAction<number>> }> = ({
+  state,
+  setState,
+}) => {
+  console.log("Inner2_2 redraw");
+  return (
+    <div>
+      <h3>Inner2_2</h3>
+      <button onClick={() => setState((prev) => prev + 1)}>increment</button>
+      <p>{state}</p>
+    </div>
+  );
+};
+
 const Inner3Wrapper = () => {
   console.log("Inner3Wrapper redraw");
   const stateAtom = useMemo(
@@ -61,16 +99,48 @@ const Inner3Wrapper = () => {
       }),
     []
   );
+  const setState = useSetRecoilState(stateAtom);
 
-  return <Inner3 stateAtom={stateAtom} />;
+  return <Inner3 setState={setState} stateAtom={stateAtom} />;
 };
 
-const Inner3: FC<{ stateAtom: RecoilState<number> }> = ({ stateAtom }) => {
+const Inner3: FC<{ setState: SetterOrUpdater<number>; stateAtom: RecoilState<number> }> = ({
+  setState,
+  stateAtom,
+}) => {
   console.log("Inner3 redraw");
+  const state = useRecoilValue(stateAtom);
+
+  return (
+    <div>
+      <h3>Inner3</h3>
+      <button onClick={() => setState((prev) => prev + 1)}>increment</button>
+      <p>{state}</p>
+    </div>
+  );
+};
+
+const Inner3_2Wrapper = () => {
+  console.log("Inner3_2Wrapper redraw");
+  const stateAtom = useMemo(
+    () =>
+      atom({
+        key: `${new Date()}/${Math.random()}`,
+        default: 0,
+      }),
+    []
+  );
+
+  return <Inner3_2 stateAtom={stateAtom} />;
+};
+
+const Inner3_2: FC<{ stateAtom: RecoilState<number> }> = ({ stateAtom }) => {
+  console.log("Inner3_2 redraw");
   const [state, setState] = useRecoilState(stateAtom);
 
   return (
     <div>
+      <h3>Inner3_2</h3>
       <button onClick={() => setState((prev) => prev + 1)}>increment</button>
       <p>{state}</p>
     </div>
@@ -92,6 +162,7 @@ const Inner4 = () => {
   const [state, setState] = useRecoilState(IndependentAtom);
   return (
     <div>
+      <h3>Inner4</h3>
       <button onClick={() => setState((prev) => prev + 1)}>increment</button>
       <p>{state}</p>
     </div>
